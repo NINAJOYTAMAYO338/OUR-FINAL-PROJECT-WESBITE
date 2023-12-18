dc48bd32444a972a2d6c62ecc229e024aebc1b1d
NINAJOYTAMAYO338 <154109815+NINAJOYTAMAYO338@users.noreply.github.com>
2023-12-18 11:36:41 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+var i = document.getElementById("nav");
+
+        function menu() {
+            if (i.style.display === "flex") {
+                i.style.display = "none";
+            } 
+            else {
+                i.style.display = "flex";
+            }
+        }
